cf3bf48fe58d05e536e69d872685b95ad143b6e2
Muhsin Ali <muhsinali@users.noreply.github.com>
2016-10-23 23:16:01 +0100

    Add additional functional tests

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,4 +1,11 @@
+import java.io.File
+
+import com.google.common.io.Files
+import models.Place
 import org.scalatestplus.play._
+import play.api.libs.Files.TemporaryFile
+import play.api.mvc.MultipartFormData
+import play.api.mvc.MultipartFormData.{DataPart, FilePart}
 import play.api.test._
 import play.api.test.Helpers._
 
@@ -10,18 +17,15 @@ import play.api.test.Helpers._
 class ApplicationSpec extends PlaySpec with OneAppPerSuite {
 
   "Routes" should {
-
     "send 404 on a bad request" in  {
       route(app, FakeRequest(GET, "/boum")).map(status(_)) mustBe Some(NOT_FOUND)
     }
-
   }
 
   "Application" should {
     // Application.showGridView tests
     "render the grid view" in {
       val gridView = route(app, FakeRequest(GET, "/")).get
-
       status(gridView) mustBe OK
       contentAsString(gridView) must include ("Grid view")
     }
@@ -39,7 +43,6 @@ class ApplicationSpec extends PlaySpec with OneAppPerSuite {
       status(london) mustBe OK
       contentAsString(london) must include ("London")
     }
-
     "fail on non-existent place" in {
       val nonExistentPlace = route(app, FakeRequest(GET, "/show/9999")).get
       status(nonExistentPlace) mustBe SEE_OTHER
@@ -59,6 +62,43 @@ class ApplicationSpec extends PlaySpec with OneAppPerSuite {
     "fail on non-existent picture" in {
       val pictureOfNonExistentPlace = route(app, FakeRequest(GET, "/picture/9999")).get
       status(pictureOfNonExistentPlace) mustBe BAD_REQUEST
+    }
+
+
+    // TODO add a mock place, and edit it and then delete it.
+//    // Doesn't work - TODO figure out how to add a mock place
+//    "add a mock place" in {
+//      val placeData: Map[String, Seq[String]] = Map(
+//        "id" -> Seq("0"),
+//        "name" -> Seq("mockName"),
+//        "country" -> Seq("mockCountry"),
+//        "description" -> Seq("mockDescription")
+//      )
+//
+//      val tempFile = TemporaryFile(new File("./public/images/favicon.png"))
+//      val part = FilePart[TemporaryFile]("picture", "./public/images/favicon.png", Some("image/jpeg"), tempFile)
+//      val formData = MultipartFormData(dataParts = placeData, files = Seq(part), badParts = Seq())
+//      val addResult = route(FakeRequest(POST, "/add", FakeHeaders(), formData)).get
+//
+//
+//      println(addResult)
+//      status(addResult) mustBe SEE_OTHER
+//    }
+
+
+    // Application.editPlace() tests
+    "edit London place" in {
+      val editLondon = route(app, FakeRequest(GET, "/edit/1")).get
+      status(editLondon) mustBe OK
+    }
+
+    // Application.deletePlace() tests
+    "delete London place" in {
+      val deleteLondon = route(app, FakeRequest(DELETE, "/delete/1")).get
+      status(deleteLondon) mustBe SEE_OTHER
+      // TODO find out why deleteLondon is empty
+//      println(contentAsString(deleteLondon))
+//      contentAsString(deleteLondon) must include ("Deleted place with ID")
     }
 
   }
